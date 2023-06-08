750d8daa133666f9d87781025ff03e59c4b9ac72
Alex Silva <alexcssilva@gmail.com>
2023-06-07 22:41:18 -0300

    feat: :sparkles: Added View - FormView and Component related LicitationForm

---
+++ src/views/FormView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <LicitationForm />
+  </div>
+</template>
+
+<script>
+
+import LicitationForm from '@/components/LicitationForm.vue';
+
+export default {
+  name: 'FormView',
+  components: {
+    LicitationForm
+  }
+}
+</script>
