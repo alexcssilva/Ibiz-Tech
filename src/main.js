339b0b9e044219e24710146e61afe1d2399eb443
Alex Silva <alexcssilva@gmail.com>
2023-06-11 14:52:31 -0300

    feat: :alien: Implemented API integration file globally axios.js and imported to main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './plugins/axios'
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./styles.css";
