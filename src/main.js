25cef40213b5138de30b171a9d01eeb9aaf3b0a5
Alex Silva <alexcssilva@gmail.com>
2023-06-17 19:03:58 -0300

    build: :package: Moment method import to format datetime

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import moment from "moment";
 import './plugins/axios'
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./styles.css";
 
 Vue.config.productionTip = false
+
+Vue.prototype.$moment = moment;
 
 new Vue({
   router,
