adda57b547c59b35a4faf031091a05165f26b5e7
Alex Silva <alexcssilva@gmail.com>
2023-06-19 15:15:04 -0300

    test: :white_check_mark: Mock created from a licitation

---
+++ tests/unit/mocks/mockLicitation.js
@@ -0,0 +1,16 @@
+const mockLicitation = {
+  id_lic: 1,
+  nu_fase: 0,
+  nu_edital: "12345",
+  modalidade: "Leilão",
+  data_abertura: "2023-06-01",
+  nome_licitador: null,
+  cnpj_licitador: null,
+  prioridade: 1,
+  objeto: "Objeto da licitação",
+  created_at: "2023-06-01",
+  updated_at: "2023-06-01",
+  deleted_at: null,
+};
+
+export default mockLicitation;
